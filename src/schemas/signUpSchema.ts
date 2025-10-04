95cb9173bb6fd65d73d0de5862da95aa5be98685
Satyam Lal <ssatyamlal@gmail.com>
2025-10-05 00:25:40 +0530

    signUpSchema completed

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,15 @@
+import { z } from "zod/v4";
+
+export const usernameValidation = z
+  .string()
+  .min(5, "Username must be atleast 5 characters!")
+  .max(20, "Username can't exceed 20 characters!")
+  .regex(/^[a-zA-Z0-9._-]+$/, "Username must not contain special characters!");
+
+export const signUpSchema = z.object({
+  username: usernameValidation,
+  email: z.string().email({ error: "Invalid email address." }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters" }),
+});
